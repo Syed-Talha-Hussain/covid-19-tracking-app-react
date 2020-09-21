520f8869e1f498a65128e9a6cd0e8c07f79ffdd6
SyedTalhaHussain <talhahussain570@gmail.com>
2020-09-21 20:31:18 +0500

    My Final Covid-19 Tracking App on React

---
+++ precache-manifest.be721893d5cdf66ecf65e7a3050f5fbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542aa3b9da3710315da080ea08bf9867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aaaa8e6a74151c290bf",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "5542c29b267a4d6af721",
+    "url": "/static/css/main.b0fed035.chunk.css"
+  },
+  {
+    "revision": "9aaaa8e6a74151c290bf",
+    "url": "/static/js/2.7de38c10.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.7de38c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5542c29b267a4d6af721",
+    "url": "/static/js/main.2ba9b450.chunk.js"
+  },
+  {
+    "revision": "57bad9ac21a6be00b5d6",
+    "url": "/static/js/runtime-main.d138b4fd.js"
+  }
+]);
